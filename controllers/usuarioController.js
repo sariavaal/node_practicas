97b8ac0fa102b8c3be5aad69574c45aba216a580
sariavaal <sariavaal@gmail.com>
2023-09-05 17:20:12 -0400

    validaciones de formulario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,6 @@
+import {check, validationResult} from 'express-validator'
 import Usuario from '../models/Usuario.js'
+
 
 
 const formularioLogin = (req, res) => {
@@ -17,7 +19,29 @@ const formularioRegistro = (req, res) => {
 }
 
 const registrar = async (req, res) => {
+    console.log(req.body)
+    //validacion
+    await check('nombre').notEmpty().withMessage('El nombre no puede ir vacío').run(req)
+    await check('email').isEmail().withMessage('Email inválido').run(req)
+    await check ('password').isLength({min: 8}).withMessage('El password debe ser de al menos 8 caracteres').run(req)
+    await check ('repetir_password').equals('password').withMessage('Los passwords no son iguales').run(req)
+
+    let resultado = validationResult(req)
+
+
+   // return res.json({errores: resultado.array()})
+    //verificar que el resultado este vacio
+    if(!resultado.isEmpty()) {
+        //errores
+        return res.render('auth/registro', {
+            pagina: 'Crear Cuenta',
+            errores: resultado.array()
+        })
+
+    }
+
     
+
     const usuario = await Usuario.create(req.body);
 
     res.json(usuario)
