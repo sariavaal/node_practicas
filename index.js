ec7e77a4a856a37795fcc79c58bf93ce6a0bda6d
sariavaal <sariavaal@gmail.com>
2023-09-12 08:49:36 -0400

    agregado pagina de mis propiedades

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import csrf from 'csurf'
 import cookieParser from 'cookie-parser'
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import propiedadesRoutes from './routes/propiedadesRoutes.js'
 import db from './config/db.js'
 
 //crear la app
@@ -41,6 +42,8 @@ app.use( express.static('public'))
 
 //routing
 app.use('/auth', usuarioRoutes)
+app.use('/', propiedadesRoutes)
+
 
 
 
---
+++ routes/propiedadesRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import{ admin } from '../controllers/propiedadController.js'
+
+const router = express.Router()
+
+router.get('/mis-propiedades', admin)
+
+
+export default router
