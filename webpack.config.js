09b86922c5f7472f7fa8d49dc7e8c5de97d096d4
sariavaal <sariavaal@gmail.com>
2023-09-12 10:49:07 -0400

    webpack configuraciones

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+import path from 'path'
+
+export default {
+    mode: 'development',
+    entry:{
+        mapa: './src/js/mapa.js'
+
+    },
+    output:{
+        filename: '[name].js',
+        path: path.resolve('public/js')
+
+    }
+}
