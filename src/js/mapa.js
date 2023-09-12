5cfdf41b5f3618012dc3572da9f8637f57f22448
sariavaal <sariavaal@gmail.com>
2023-09-12 15:50:03 -0400

    mapa con cursor y nombre de la calle

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -3,6 +3,9 @@
     const lng = -55.8604449;
     const mapa = L.map('mapa').setView([lat, lng ], 30);
     let marker;
+
+    //utilizar provider y geocoder
+    const geocodeService = L.esri.Geocoding.geocodeService();
     
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -21,6 +24,22 @@
         marker = e.target
         const posicion = marker.getLatLng();
         mapa.panTo(new L.LatLng(posicion.lat, posicion.lng ))
+
+        //obtener informacion de las calles
+        geocodeService.reverse().latlng(posicion, 20).run(function(error, resultado){
+           // console.log(resultado)
+
+            marker.bindPopup(resultado.address.LongLabel)
+
+            //llenar los campos
+            document.querySelector('.calle').textContent = resultado?.address?.Address ?? '';
+            document.querySelector('#calle').value = resultado?.address?.Address ?? '';
+            document.querySelector('#lat').value = resultado?.latlng?.lat ?? '';
+            document.querySelector('#lng').value = resultado?.latlng?.lng ?? '';
+        })
+
+
+
     })
 
 
